0f7a1fa5c0e6ffbf60a5063c96aa0de9727287be
kevinheavey <kevinheavey123@gmail.com>
2021-12-29 13:52:11 +0000

    make pure rust

--- sol_rust_py/__init__.py
+++
@@ -1,4 +0,0 @@
-from sol_rust_py.sol_rust_py import sum_as_string, is_on_curve
-
-
-__all__ = ["sum_as_string", "is_on_curve"]
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,14 +1,17 @@
+from pytest import raises
 from sol_rust_py import is_on_curve
 
 
 def test_is_on_curve(benchmark):
-    # data = (
-    #     b"\xc1M\xce\x1e\xa4\x86<\xf1\xbc\xfc\x12\xf4\xf2\xe2Y"
-    #     b"\xf4\x8d\xe4V\xb7\xf9\xd4\\!{\x04\x89j\x1f\xfeA\xdc"
-    # )
     data = (
         b"\xc1M\xce\x1e\xa4\x86<\xf1\xbc\xfc\x12\xf4\xf2\xe2Y"
-        # b"\xf4\x8d\xe4V\xb7\xf9\xd4\\!{\x04\x89j\x1f\xfeA\xdc"
+        b"\xf4\x8d\xe4V\xb7\xf9\xd4\\!{\x04\x89j\x1f\xfeA\xdc"
     )
     result = benchmark(is_on_curve, data)
     assert result
+
+
+def test_is_on_curve_wrong_length():
+    data = b"\xc1M"
+    with raises(BaseException):
+        is_on_curve(data)
