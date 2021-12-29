061f33e91ff3d165a2c2526188dc27ad4c89a594
kevinheavey <kevinheavey123@gmail.com>
2021-12-29 04:05:17 +0000

    init files

---
+++ sol_rust_py/__init__.py
@@ -0,0 +1,4 @@
+from sol_rust_py.sol_rust_py import sum_as_string, is_on_curve
+
+
+__all__ = ["sum_as_string", "is_on_curve"]
---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+use pyo3::prelude::*;
+use solana_sdk::pubkey::bytes_are_curve_point;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+#[pyfunction]
+fn is_on_curve(_bytes: &[u8]) -> bool {
+    bytes_are_curve_point(_bytes)
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn sol_rust_py(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(is_on_curve, m)?)?;
+    Ok(())
+}
