35e2ac12318e6367907743cec38b488d99fe455b
Kevin Heavey <kevinheavey123@gmail.com>
2022-05-17 12:16:59 +0100

    better sysvar imports

--- src/sysvar.rs
+++ src/sysvar.rs
@@ -1,42 +1,21 @@
 use crate::Pubkey;
 use pyo3::prelude::*;
-use solana_sdk::pubkey;
+use solana_sdk::sysvar as sysvar_original;
 
 pub fn create_sysvar_mod(py: Python<'_>) -> PyResult<&PyModule> {
     let sysvar_mod = PyModule::new(py, "sysvar")?;
     let sysvars = vec![
-        (
-            "CLOCK",
-            pubkey!("SysvarC1ock11111111111111111111111111111111"),
-        ),
+        ("CLOCK", sysvar_original::clock::ID),
         (
             "RECENT_BLOCKHASHES",
-            pubkey!("SysvarRecentB1ockHashes11111111111111111111"),
+            sysvar_original::recent_blockhashes::ID,
         ),
-        (
-            "RENT",
-            pubkey!("SysvarRent111111111111111111111111111111111"),
-        ),
-        (
-            "REWARDS",
-            pubkey!("SysvarRewards111111111111111111111111111111"),
-        ),
-        (
-            "STAKE_HISTORY",
-            pubkey!("SysvarStakeHistory1111111111111111111111111"),
-        ),
-        (
-            "EPOCH_SCHEDULE",
-            pubkey!("SysvarEpochSchedu1e111111111111111111111111"),
-        ),
-        (
-            "INSTRUCTIONS",
-            pubkey!("Sysvar1nstructions1111111111111111111111111"),
-        ),
-        (
-            "SLOT_HASHES",
-            pubkey!("SysvarS1otHashes111111111111111111111111111"),
-        ),
+        ("RENT", sysvar_original::rent::ID),
+        ("REWARDS", sysvar_original::rewards::ID),
+        ("STAKE_HISTORY", sysvar_original::stake_history::ID),
+        ("EPOCH_SCHEDULE", sysvar_original::epoch_schedule::ID),
+        ("INSTRUCTIONS", sysvar_original::instructions::ID),
+        ("SLOT_HASHES", sysvar_original::slot_hashes::ID),
     ];
     for sysvar in sysvars {
         sysvar_mod.add(sysvar.0, Pubkey(sysvar.1))?
