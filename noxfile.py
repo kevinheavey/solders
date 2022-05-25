afea3858323694fab8902eb9cd1be15bb413912c
Kevin Heavey <kevinheavey123@gmail.com>
2022-05-25 20:33:35 +0100

    add noxfile

---
+++ noxfile.py
@@ -0,0 +1,9 @@
+# type: ignore
+import nox
+
+
+@nox.session
+def python(session):
+    session.install("pytest", "maturin", "sphinx", "based58", "pybip39")
+    session.install(".", "--no-build-isolation")
+    session.run("make", "test", external=True)
