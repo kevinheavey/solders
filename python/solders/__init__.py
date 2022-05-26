e6ab1dec19ffe11e7b71e5646a05e4ac6009a5b1
Kevin Heavey <kevinheavey123@gmail.com>
2022-05-26 20:20:46 +0100

    add __all__

--- python/solders/__init__.py
+++ python/solders/__init__.py
@@ -9,6 +9,21 @@ from solders.solders import (
     pubkey,
     signature,
     transaction,
-)  # type: ignore
+)
 from . import sysvar
 from . import system_program
+
+__all__ = [
+    "errors",
+    "hash",
+    "instruction",
+    "keypair",
+    "message",
+    "null_signer",
+    "presigner",
+    "pubkey",
+    "signature",
+    "transaction",
+    "sysvar",
+    "system_program",
+]
