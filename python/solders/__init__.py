1709ebee76c1fb2ff32eac1805e4e2670a222a3b
Kevin Heavey <kevinheavey123@gmail.com>
2022-05-26 20:15:12 +0100

    try fix layout

---
+++ python/solders/__init__.py
@@ -0,0 +1,14 @@
+from solders.solders import (
+    errors,
+    hash,
+    instruction,
+    keypair,
+    message,
+    null_signer,
+    presigner,
+    pubkey,
+    signature,
+    transaction,
+)  # type: ignore
+from . import sysvar
+from . import system_program
--- solders/__init__.py
+++
@@ -1,3 +0,0 @@
-from solders import solders  # type: ignore
-from . import system_program
-from . import sysvar
