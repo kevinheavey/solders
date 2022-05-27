f02f56465cb6567ad0f21bb25ca30bbc98dcce57
Kevin Heavey <kevinheavey123@gmail.com>
2022-05-27 09:42:05 +0100

    missing installs

--- noxfile.py
+++ noxfile.py
@@ -4,6 +4,8 @@ import nox
 
 @nox.session
 def python(session):
-    session.install("pytest", "maturin", "sphinx", "based58", "pybip39")
+    session.install(
+        "pytest", "maturin", "sphinx", "based58", "pybip39", "typing-extensions"
+    )
     session.install(".", "--no-build-isolation")
     session.run("make", "test", external=True)
