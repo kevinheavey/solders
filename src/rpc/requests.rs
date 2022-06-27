ba89b1cbfcda470b784c798744f6706cc2b68898
Kevin Heavey <kevinheavey123@gmail.com>
2022-06-27 14:48:44 +0100

    more request builder rough work

--- src/rpc/requests.rs
+++ src/rpc/requests.rs
@@ -1,79 +1,148 @@
-use jsonrpc_core::{
-    types::{Id, MethodCall, Params},
-    Version,
+use std::fmt::Display;
+
+use crate::{
+    py_from_bytes_general_via_bincode, pybytes_general_via_bincode, CommonMethods, Pubkey,
+    PyBytesBincode, PyFromBytesBincode, RichcmpEqualityOnly,
 };
 use pyo3::prelude::*;
 use serde::{Deserialize, Serialize};
+use serde_with::{serde_as, DisplayFromStr};
+use solders_macros::{common_magic_methods, richcmp_eq_only};
 
 use crate::Signature;
 
-use super::config::RpcSignatureStatusConfig;
+use super::config::{RpcRequestAirdropConfig, RpcSignatureStatusConfig};
 
-macro_rules! rpc_params {
-	($($param:expr),*) => {
-		{
-			let mut __params = vec![];
-			$(
-				__params.push(serde_json::to_value($param).unwrap());
-			)*
-			Params::Array(__params)
-		}
-	};
-	() => {
-		Params::None
-	}
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq)]
+struct RequestBase {
+    jsonrpc: String,
+    id: u64,
+    method: String,
 }
 
-fn method_call(id: Option<u64>, method: String, params: Params) -> MethodCall {
-    MethodCall {
-        jsonrpc: Some(Version::V2),
-        id: Id::Num(id.unwrap_or(0)),
-        method,
-        params,
+impl RequestBase {
+    fn new(method: String, id: Option<u64>) -> Self {
+        Self {
+            jsonrpc: "2.0".into(),
+            id: id.unwrap_or(0),
+            method,
+        }
     }
 }
 
 #[pyclass(module = "solders.rpc.requests")]
 #[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
-pub struct GetSignatureStatuses(MethodCall);
+pub struct GetSignatureStatuses {
+    #[serde(flatten)]
+    base: RequestBase,
+    #[pyo3(get)]
+    params: GetSignatureStatusesParams,
+}
 
+#[richcmp_eq_only]
+#[common_magic_methods]
 #[pymethods]
 impl GetSignatureStatuses {
     #[new]
-    fn new(signatures: Vec<Signature>, config: RpcSignatureStatusConfig, id: Option<u64>) -> Self {
-        Self(method_call(
-            id,
-            "getSignatureStatuses".to_owned(),
-            rpc_params![
-                signatures.iter().map(|s| s.to_string()).collect::<Vec<_>>(),
-                config
-            ],
-        ))
+    fn new(
+        signatures: Vec<Signature>,
+        config: Option<RpcSignatureStatusConfig>,
+        id: Option<u64>,
+    ) -> Self {
+        let params = GetSignatureStatusesParams(signatures, config);
+        let method = "getSignatureStatuses".to_owned();
+        let base = RequestBase::new(method, id);
+        Self { base, params }
     }
 
-    /// Convert to JSON
-    ///
-    /// Example:
-    ///     >>> from solders.rpc.requests import GetSignatureStatuses
-    ///     >>> from solders.signature import Signature
-    ///     >>> from solders.rpc.config import RpcSignatureStatusConfig
-    ///     >>> req = GetSignatureStatuses([Signature.default()], RpcSignatureStatusConfig(True))
-    ///     >>> req.to_json()
-    ///     '{"jsonrpc":"2.0","method":"getSignatureStatuses","params":[["1111111111111111111111111111111111111111111111111111111111111111"],{"searchTransactionHistory":true}],"id":0}'
-    ///
     fn to_json(&self) -> String {
         serde_json::to_string(self).unwrap()
     }
 
     #[staticmethod]
     fn from_json(raw: &str) -> Self {
-        let underlying: MethodCall = serde_json::from_str(raw).unwrap();
-        Self(underlying)
+        serde_json::from_str(raw).unwrap()
     }
 }
+
+impl Display for GetSignatureStatuses {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.to_json())
+    }
+}
+
+impl CommonMethods for GetSignatureStatuses {}
+impl RichcmpEqualityOnly for GetSignatureStatuses {}
+pybytes_general_via_bincode!(GetSignatureStatuses);
+py_from_bytes_general_via_bincode!(GetSignatureStatuses);
+
+#[serde_as]
+#[pyclass(module = "solders.rpc.requests")]
+#[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
+pub struct GetSignatureStatusesParams(
+    #[serde_as(as = "Vec<DisplayFromStr>")] Vec<Signature>,
+    #[serde(skip_serializing_if = "Option::is_none", default)] Option<RpcSignatureStatusConfig>,
+);
+
+#[serde_as]
+#[pyclass(module = "solders.rpc.requests")]
+#[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
+pub struct RequestAirdropParams(
+    #[serde_as(as = "DisplayFromStr")] Pubkey,
+    u64,
+    #[serde(skip_serializing_if = "Option::is_none", default)] Option<RpcRequestAirdropConfig>,
+);
+
+#[pyclass(module = "solders.rpc.requests")]
+#[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
+pub struct RequestAirdrop {
+    #[serde(flatten)]
+    base: RequestBase,
+    #[pyo3(get)]
+    params: RequestAirdropParams,
+}
+
+#[richcmp_eq_only]
+#[common_magic_methods]
+#[pymethods]
+impl RequestAirdrop {
+    #[new]
+    fn new(
+        pubkey: Pubkey,
+        lamports: u64,
+        config: Option<RpcRequestAirdropConfig>,
+        id: Option<u64>,
+    ) -> Self {
+        let params = RequestAirdropParams(pubkey, lamports, config);
+        let method = "requestAirdrop".to_owned();
+        let base = RequestBase::new(method, id);
+        Self { base, params }
+    }
+
+    fn to_json(&self) -> String {
+        serde_json::to_string(self).unwrap()
+    }
+
+    #[staticmethod]
+    fn from_json(raw: &str) -> Self {
+        serde_json::from_str(raw).unwrap()
+    }
+}
+
+impl Display for RequestAirdrop {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.to_json())
+    }
+}
+
+impl CommonMethods for RequestAirdrop {}
+impl RichcmpEqualityOnly for RequestAirdrop {}
+pybytes_general_via_bincode!(RequestAirdrop);
+py_from_bytes_general_via_bincode!(RequestAirdrop);
 
 pub fn create_requests_mod(py: Python<'_>) -> PyResult<&PyModule> {
     let requests_mod = PyModule::new(py, "requests")?;
     requests_mod.add_class::<GetSignatureStatuses>()?;
+    requests_mod.add_class::<RequestAirdrop>()?;
     Ok(requests_mod)
 }
