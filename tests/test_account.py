a517f8dba588c3db20fdedea707e8975b83c9635
Kevin Heavey <kevinheavey123@gmail.com>
2022-07-06 16:15:41 +0100

    add tests

---
+++ tests/test_account.py
@@ -0,0 +1,27 @@
+from pytest import fixture
+import pickle
+from based58 import b58encode, b58decode
+from pytest import raises, fixture
+from solders.signature import Signature
+from solders.keypair import Keypair
+from solders.pubkey import Pubkey
+from solders.account import Account
+
+
+@fixture
+def account() -> Account:
+    return Account(1, b"123", Pubkey.default(), True, 1)
+
+
+def test_bytes(account: Account) -> None:
+    assert Account.from_bytes(bytes(account))
+
+
+def test_pickle(account: Account) -> None:
+    obj = Account.default()
+    assert pickle.loads(pickle.dumps(obj)) == obj
+
+
+def test_json() -> None:
+    obj = Account.default()
+    assert Account.from_json(obj.to_json()) == obj
