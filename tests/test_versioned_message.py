55e4cd6a5c2e8209b0b62f977d08afb7c2143abd
Kevin Heavey <kevinheavey123@gmail.com>
2022-07-11 08:53:10 +0100

    fix types

--- tests/test_versioned_message.py
+++ tests/test_versioned_message.py
@@ -16,6 +16,7 @@ def default_header_with_one_req_signature() -> MessageHeader:
         num_readonly_unsigned_accounts=default.num_readonly_unsigned_accounts,
     )
 
+
 @fixture
 def default_message() -> MessageV0:
     return MessageV0.default()
@@ -89,44 +90,41 @@ def test_sanitize_with_table_lookup_and_ix_with_dynamic_program_id(
     )
     message.sanitize(False)
     with raises(SanitizeError):
-        assert message.sanitize(True)
+        message.sanitize(True)
 
 
 def test_sanitize_with_table_lookup_and_ix_with_static_program_id(
     default_header_with_one_req_signature: MessageHeader, default_message: MessageV0
 ) -> None:
-    assert (
-        MessageV0(
-            header=default_header_with_one_req_signature,
-            account_keys=[Pubkey.new_unique(), Pubkey.new_unique()],
-            address_table_lookups=[
-                MessageAddressTableLookup(
-                    account_key=Pubkey.new_unique(),
-                    writable_indexes=bytes([1, 2, 3]),
-                    readonly_indexes=bytes([0]),
-                )
-            ],
-            instructions=[
-                CompiledInstruction(
-                    program_id_index=1, accounts=bytes([2, 3, 4, 5]), data=bytes([])
-                )
-            ],
-            recent_blockhash=default_message.recent_blockhash,
-        ).sanitize(True)
-        is None
-    )
+    MessageV0(
+        header=default_header_with_one_req_signature,
+        account_keys=[Pubkey.new_unique(), Pubkey.new_unique()],
+        address_table_lookups=[
+            MessageAddressTableLookup(
+                account_key=Pubkey.new_unique(),
+                writable_indexes=bytes([1, 2, 3]),
+                readonly_indexes=bytes([0]),
+            )
+        ],
+        instructions=[
+            CompiledInstruction(
+                program_id_index=1, accounts=bytes([2, 3, 4, 5]), data=bytes([])
+            )
+        ],
+        recent_blockhash=default_message.recent_blockhash,
+    ).sanitize(True)
 
 
 def test_sanitize_without_signer(
     default_header_with_one_req_signature: MessageHeader, default_message: MessageV0
 ) -> None:
     msg = MessageV0(
-            header=MessageHeader.default(),
-            account_keys=[Pubkey.new_unique()],
-            recent_blockhash=default_message.recent_blockhash,
-            instructions=default_message.instructions,
-            address_table_lookups=default_message.address_table_lookups,
-        )
+        header=MessageHeader.default(),
+        account_keys=[Pubkey.new_unique()],
+        recent_blockhash=default_message.recent_blockhash,
+        instructions=default_message.instructions,
+        address_table_lookups=default_message.address_table_lookups,
+    )
     with raises(SanitizeError):
         msg.sanitize(True)
 
@@ -135,16 +133,16 @@ def test_sanitize_without_writable_signer(
     default_header_with_one_req_signature: MessageHeader, default_message: MessageV0
 ) -> None:
     msg = MessageV0(
-            header=MessageHeader(
-                num_required_signatures=1,
-                num_readonly_signed_accounts=1,
-                num_readonly_unsigned_accounts=MessageHeader.default().num_readonly_unsigned_accounts,
-            ),
-            account_keys=[Pubkey.new_unique()],
-            recent_blockhash=default_message.recent_blockhash,
-            instructions=default_message.instructions,
-            address_table_lookups=default_message.address_table_lookups,
-        )
+        header=MessageHeader(
+            num_required_signatures=1,
+            num_readonly_signed_accounts=1,
+            num_readonly_unsigned_accounts=MessageHeader.default().num_readonly_unsigned_accounts,
+        ),
+        account_keys=[Pubkey.new_unique()],
+        recent_blockhash=default_message.recent_blockhash,
+        instructions=default_message.instructions,
+        address_table_lookups=default_message.address_table_lookups,
+    )
     with raises(SanitizeError):
         msg.sanitize(True)
 
@@ -153,18 +151,18 @@ def test_sanitize_with_empty_table_lookup(
     default_header_with_one_req_signature: MessageHeader, default_message: MessageV0
 ) -> None:
     msg = MessageV0(
-            header=default_header_with_one_req_signature,
-            account_keys=[Pubkey.new_unique()],
-            address_table_lookups=[
-                MessageAddressTableLookup(
-                    account_key=Pubkey.new_unique(),
-                    writable_indexes=bytes([]),
-                    readonly_indexes=bytes([]),
-                )
-            ],
-            recent_blockhash=default_message.recent_blockhash,
-            instructions=default_message.instructions,
-        )
+        header=default_header_with_one_req_signature,
+        account_keys=[Pubkey.new_unique()],
+        address_table_lookups=[
+            MessageAddressTableLookup(
+                account_key=Pubkey.new_unique(),
+                writable_indexes=bytes([]),
+                readonly_indexes=bytes([]),
+            )
+        ],
+        recent_blockhash=default_message.recent_blockhash,
+        instructions=default_message.instructions,
+    )
     with raises(SanitizeError):
         msg.sanitize(True)
 
@@ -172,28 +170,26 @@ def test_sanitize_with_empty_table_lookup(
 def test_sanitize_with_max_account_keys(
     default_header_with_one_req_signature: MessageHeader, default_message: MessageV0
 ) -> None:
-    assert (
-        MessageV0(
-            header=default_header_with_one_req_signature,
-            account_keys=[Pubkey.new_unique() for i in range(256)],
-            recent_blockhash=default_message.recent_blockhash,
-            instructions=default_message.instructions,
-            address_table_lookups=default_message.address_table_lookups,
-        ).sanitize(True)
-        is None
-    )
+
+    MessageV0(
+        header=default_header_with_one_req_signature,
+        account_keys=[Pubkey.new_unique() for i in range(256)],
+        recent_blockhash=default_message.recent_blockhash,
+        instructions=default_message.instructions,
+        address_table_lookups=default_message.address_table_lookups,
+    ).sanitize(True)
 
 
 def test_sanitize_with_too_many_account_keys(
     default_header_with_one_req_signature: MessageHeader, default_message: MessageV0
 ) -> None:
     message = MessageV0(
-            header=default_header_with_one_req_signature,
-            account_keys=[Pubkey.new_unique() for i in range(257)],
-            recent_blockhash=default_message.recent_blockhash,
-            instructions=default_message.instructions,
-            address_table_lookups=default_message.address_table_lookups,
-        )
+        header=default_header_with_one_req_signature,
+        account_keys=[Pubkey.new_unique() for i in range(257)],
+        recent_blockhash=default_message.recent_blockhash,
+        instructions=default_message.instructions,
+        address_table_lookups=default_message.address_table_lookups,
+    )
     with raises(SanitizeError):
         message.sanitize(True)
 
@@ -201,40 +197,37 @@ def test_sanitize_with_too_many_account_keys(
 def test_sanitize_with_max_table_loaded_keys(
     default_header_with_one_req_signature: MessageHeader, default_message: MessageV0
 ) -> None:
-    assert (
-        MessageV0(
-            header=default_header_with_one_req_signature,
-            account_keys=[Pubkey.new_unique()],
-            address_table_lookups=[
-                MessageAddressTableLookup(
-                    account_key=Pubkey.new_unique(),
-                    writable_indexes=bytes(range(0, 255, 2)),
-                    readonly_indexes=bytes(range(1, 255, 2)),
-                )
-            ],
-            recent_blockhash=default_message.recent_blockhash,
-            instructions=default_message.instructions,
-        ).sanitize(True)
-        is None
-    )
+    MessageV0(
+        header=default_header_with_one_req_signature,
+        account_keys=[Pubkey.new_unique()],
+        address_table_lookups=[
+            MessageAddressTableLookup(
+                account_key=Pubkey.new_unique(),
+                writable_indexes=bytes(range(0, 255, 2)),
+                readonly_indexes=bytes(range(1, 255, 2)),
+            )
+        ],
+        recent_blockhash=default_message.recent_blockhash,
+        instructions=default_message.instructions,
+    ).sanitize(True)
 
 
 def test_sanitize_with_too_many_table_loaded_keys(
     default_header_with_one_req_signature: MessageHeader, default_message: MessageV0
 ) -> None:
-    message =MessageV0(
-            header=default_header_with_one_req_signature,
-            account_keys=[Pubkey.new_unique()],
-            address_table_lookups=[
-                MessageAddressTableLookup(
-                    account_key=Pubkey.new_unique(),
-                    writable_indexes=bytes(range(0, 256, 2)),
-                    readonly_indexes=bytes(range(1, 256, 2)),
-                )
-            ],
-            recent_blockhash=default_message.recent_blockhash,
-            instructions=default_message.instructions,
-        )
+    message = MessageV0(
+        header=default_header_with_one_req_signature,
+        account_keys=[Pubkey.new_unique()],
+        address_table_lookups=[
+            MessageAddressTableLookup(
+                account_key=Pubkey.new_unique(),
+                writable_indexes=bytes(range(0, 256, 2)),
+                readonly_indexes=bytes(range(1, 256, 2)),
+            )
+        ],
+        recent_blockhash=default_message.recent_blockhash,
+        instructions=default_message.instructions,
+    )
     with raises(SanitizeError):
         message.sanitize(True)
 
@@ -271,19 +264,19 @@ def test_sanitize_with_invalid_ix_account(
     default_header_with_one_req_signature: MessageHeader, default_message: MessageV0
 ) -> None:
     message = MessageV0(
-            header=default_header_with_one_req_signature,
-            account_keys=[Pubkey.new_unique(), Pubkey.new_unique()],
-            address_table_lookups=[
-                MessageAddressTableLookup(
-                    account_key=Pubkey.new_unique(),
-                    writable_indexes=bytes([]),
-                    readonly_indexes=bytes([0]),
-                )
-            ],
-            instructions=[
-                CompiledInstruction(program_id_index=1, accounts=bytes([3]), data=bytes([]))
-            ],
-            recent_blockhash=default_message.recent_blockhash,
+        header=default_header_with_one_req_signature,
+        account_keys=[Pubkey.new_unique(), Pubkey.new_unique()],
+        address_table_lookups=[
+            MessageAddressTableLookup(
+                account_key=Pubkey.new_unique(),
+                writable_indexes=bytes([]),
+                readonly_indexes=bytes([0]),
+            )
+        ],
+        instructions=[
+            CompiledInstruction(program_id_index=1, accounts=bytes([3]), data=bytes([]))
+        ],
+        recent_blockhash=default_message.recent_blockhash,
     )
     with raises(SanitizeError):
         message.sanitize(True)
@@ -293,7 +286,6 @@ def test_try_compile(
     default_header_with_one_req_signature: MessageHeader, default_message: MessageV0
 ) -> None:
     keys = [Pubkey.new_unique() for i in range(7)]
-
     payer = keys[0]
     program_id = keys[6]
     instructions = [
@@ -341,8 +333,8 @@ def test_try_compile(
         address_table_lookups=[
             MessageAddressTableLookup(
                 account_key=address_lookup_table_accounts[0].key,
-                writable_indexes=[0],
-                readonly_indexes=[1],
+                writable_indexes=bytes([0]),
+                readonly_indexes=bytes([1]),
             )
         ],
     )
