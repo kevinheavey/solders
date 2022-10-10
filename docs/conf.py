614d3bc6c786138f9aca001ff541aa9ea803c108
Kevin Heavey <ID+username@users.noreply.github.com>
2022-10-10 19:22:06 +0100

    Bump version: 0.7.0 → 0.8.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2022, Kevin Heavey"
 author = "Kevin Heavey"
 
 # The full version, including alpha/beta/rc tags
-release = "0.7.0"
+release = "0.8.0"
 
 
 # -- General configuration ---------------------------------------------------
