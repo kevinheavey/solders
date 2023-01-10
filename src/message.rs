6c337f8fc8615405e44914615280b0b5b879a47f
Kevin Heavey <kevinheavey123@gmail.com>
2023-01-10 00:31:45 +0000

    add missing imports

--- src/message.rs
+++ src/message.rs
@@ -1,6 +1,6 @@
-use pyo3::prelude::*;
+use pyo3::{prelude::*, PyTypeInfo, types::PyTuple};
 use solders_primitives::message::{
-    Message, MessageAddressTableLookup, MessageHeader, MessageV0, VersionedMessage,
+    Message, MessageAddressTableLookup, MessageHeader, MessageV0,
 };
 
 pub(crate) fn create_message_mod(py: Python<'_>) -> PyResult<&PyModule> {
