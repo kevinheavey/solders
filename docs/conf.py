1913e44ed69e6f29ccbdea8df4f8af90e289bbe4
Kevin Heavey <kevinheavey123@gmail.com>
2023-05-05 12:36:25 +0100

    Bump version: 0.14.4 → 0.15.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2022, Kevin Heavey"
 author = "Kevin Heavey"
 
 # The full version, including alpha/beta/rc tags
-release = "0.14.4"
+release = "0.15.0"
 
 
 # -- General configuration ---------------------------------------------------
