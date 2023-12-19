645a603641bf99a83bd0a3a0d4669748c871a3b7
Kevin Heavey <kevinheavey123@gmail.com>
2023-12-20 00:05:51 +0400

    usee acount fixture in more tests

--- tests/test_account.py
+++ tests/test_account.py
@@ -15,10 +15,8 @@ def test_bytes(account: Account) -> None:
 
 
 def test_pickle(account: Account) -> None:
-    obj = Account.default()
-    assert pickle.loads(pickle.dumps(obj)) == obj
+    assert pickle.loads(pickle.dumps(account)) == account
 
 
-def test_json() -> None:
-    obj = Account.default()
-    assert Account.from_json(obj.to_json()) == obj
+def test_json(account: Account) -> None:
+    assert Account.from_json(account.to_json()) == account
