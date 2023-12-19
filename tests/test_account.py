f30e8f98b6a4f2a8f25472b89f419c06a7204862
Kevin Heavey <kevinheavey123@gmail.com>
2023-12-20 00:06:36 +0400

    add test for issue '69

--- tests/test_account.py
+++ tests/test_account.py
@@ -20,3 +20,16 @@ def test_pickle(account: Account) -> None:
 
 def test_json(account: Account) -> None:
     assert Account.from_json(account.to_json()) == account
+
+def test_program_notification_issue_69() -> None:
+    # https://github.com/kevinheavey/solders/issues/69
+    raw = """{
+    "lamports": 16258560,
+    "data": "error: data too large for bs58 encoding",
+    "owner": "675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8",
+    "executable": false,
+    "rentEpoch": 0,
+    "space": 2208
+}
+    """
+    assert Account.from_json(raw).rent_epoch == 0
